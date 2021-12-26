25ad9c53afe662fa1d1f76655fb1c7578b07b6c2
Anil James <69676080+aniljames@users.noreply.github.com>
2021-12-26 10:53:16 +0530

    Add files via upload

---
+++ Audio Processing/audio.m
@@ -0,0 +1,9 @@
+﻿[x,fs]=audioread('10 second gaming intro music__ best ever__.mp4');
+X=x(:,2);
+len=length(X);
+t=seconds((0:len-1)/fs);
+plot(t,X)
+grid on;
+xlabel("Time")
+ylabel("Amplitude")
+title("Plot of Audio in Time Domain")
